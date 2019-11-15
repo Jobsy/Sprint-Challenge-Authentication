b52dcc19307b5e1afbf6be586fe59717e5f3872c
Oluwajoba <oluwajobabello@gmail.com>
2019-11-15 09:56:30 +0100

    update auth-router.js / post(/login)

--- jokes/jokes-model.js
+++ jokes/jokes-model.js
@@ -2,18 +2,23 @@
 const db = require('../database/dbConfig.js');
 
 module.exports = {
-  add,
-  findById
+    add,
+    findBy,
+    findById
 };
 
 async function add(user) {
-  const [id] = await db('users').insert(user);
+    const [id] = await db('users').insert(user);
 
-  return findById(id);
+    return findById(id);
 }
 
 function findById(id) {
     return db('users')
-      .where({ id })
-      .first();
-  }
+        .where({ id })
+        .first();
+}
+
+function findBy(filter) {
+    return db('users').where(filter);
+}
