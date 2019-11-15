2f13adbc0a88c941ca6295627066c660eb827776
Oluwajoba <oluwajobabello@gmail.com>
2019-11-15 15:04:53 +0100

    create and update axicomponents/container.js

---
+++ client/jokes/src/components/container.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Route, NavLink, withRouter } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import UsersList from "./UsersList";
+import { withAuthCkeck } from "./withAuthCkeck";
+
+export function Container(props) {
+  const onLogout = () => {
+    localStorage.clear();
+    props.history.push("/");
+  } 
+
+  return (
+    <div>
+      <nav>
+        <span>
+          <NavLink exact to="/">Login</NavLink><br />
+          <NavLink to="/friends">Friends List</NavLink><br />
+        </span>
+
+        <button onClick={onLogout}>Logout</button>
+
+      </nav>
+
+      <main>
+        <Route
+          exact
+          path="/"
+          component={LoginForm}
+        />
+
+        <Route
+          exact
+          path="/friends"
+          render={props => withAuthCkeck(UsersList, props)}
+        />
+      </main>
+    </div>
+  );
+}
+
+export default withRouter(Container);
