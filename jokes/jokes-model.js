49910167e5020107c2ca350f4c87e80690af12ac
Oluwajoba <oluwajobabello@gmail.com>
2019-11-15 09:52:01 +0100

    create and update jokes-model.js

---
+++ jokes/jokes-model.js
@@ -0,0 +1,19 @@
+
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+  add,
+  findById
+};
+
+async function add(user) {
+  const [id] = await db('users').insert(user);
+
+  return findById(id);
+}
+
+function findById(id) {
+    return db('users')
+      .where({ id })
+      .first();
+  }
