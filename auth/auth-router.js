406d3ceaa28d59fb1aa23bc09d4026c656b6af36
Oluwajoba <oluwajobabello@gmail.com>
2019-11-15 09:53:06 +0100

    update auth-router.js / post(/register)

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -7,7 +7,7 @@ const Users = require('../jokes/jokes-model');
 router.post('/register', (req, res) => {
   // implement registration
   let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+    const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
 
     Users.add(user)
